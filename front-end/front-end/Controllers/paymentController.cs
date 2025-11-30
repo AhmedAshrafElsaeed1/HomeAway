0d3deb985011663a18525392c02fcd54129aca60
hoda adel <hoda.adel55304@gmail.com>
2025-11-30 23:00:33 +0200

    payment added

---
+++ front-end/front-end/Controllers/paymentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace front_end.Controllers
+{
+    public class paymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
